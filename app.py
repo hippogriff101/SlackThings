5a1f7916743267fdf2afbb61e0db56f5c06f2b53
Freddie Yershon <github@freddieyershon.co.uk>
2026-08-10 20:50:42 +0100

    bearly works

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import os
-import requests
+import os, re, requests
+
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from dotenv import load_dotenv
@@ -7,16 +7,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = App(token=os.getenv("SLACK_BOT_TOKEN"))
+pattern = r"<@[^|]+\|[^>]+>" # ai assisted regex
+
 
 @app.command("/count")
-def streak_command(ack, respond, command):
+def member_count(ack, respond, command):
     ack()
     channel_name = command.get("text")
     print(f"someone is trying to look up {channel_name}")
     if channel_name.startswith('#'):
         trimname = channel_name[1:]
-    else:
-        pass
     try:
         r = requests.get(
             f"https://flaron.halceon.dev/cname/%23{trimname}",
@@ -49,6 +49,33 @@ def streak_command(ack, respond, command):
     except Exception:
         print("An unexpected error occurred.")
         respond("Unexpected error occurred. Makse sure you use the full channel name as a link, like <#freddies-castle>, oh, you should join btw")
+
+@app.command("/promote")
+def promote_mcg(ack, respond, command):
+    ack()
+    selected_user = command.get("text")
+    print(f"someone is trying to promote {selected_user}")
+    selected_id = selected_user.split("@")[1].split("|")[0]    
+    respond(f"Got it, attempting to promote {selected_id}")
+    try:
+            r = requests.get(
+                f"https://flaron.halceon.dev/promote/{selected_id}",
+                headers={"accept": "application/json"}
+            )
+
+            if r.status_code != 200:
+                print(f"Flaron API error: {r.status_code}")
+                respond(f"Flaron API error ({r.status_code}). idk what happened.")
+                return
+            
+            print("another happy customer")
+    except requests.exceptions.RequestException:
+        print("Network error while connecting to Flaron API.")
+        respond("Could not connect to Flaron API. womp womp, go ping @freddie to fix his wifi or check status.freddieyershon.co.uk if i ever make ts")
+    except Exception:
+        print("An unexpected error occurred.")
+        respond("Unexpected error occurred. :sob:")
+
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, os.getenv("SLACK_APP_TOKEN"))
